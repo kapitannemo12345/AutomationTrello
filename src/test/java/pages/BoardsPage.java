0a7e91543d9c8beac6d87986459d971fa6c3fc53
Piotr <wrobel.piotr1999@gmail.com>
2026-03-19 16:40:06 +0100

    BoardsPage refactor

--- src/test/java/pages/BoardsPage.java
+++ src/test/java/pages/BoardsPage.java
@@ -1,47 +1,52 @@
 package pages;
 
+import base.BasePage;
 import io.qameta.allure.Allure;
 import locators.BoardsPageLocators;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class BoardsPage extends BoardsPageLocators  {
+public class BoardsPage extends BasePage {
 
-    private final WebDriver driver;
-    private final WebDriverWait wait;
+    private final By createButton = By.xpath("//*[@id='header']//button//p[normalize-space(text())='Utwórz']");
+    private final By selectBoardType = By.xpath("//*[contains(text(), 'Utwórz tablicę')]");
+    private final By tableNameInputField = By.xpath("//section//form//div[1]//input");
+    private final By createBoard = By.xpath("//form//button[normalize-space(text())='Utwórz']");
+    private final By tableName = By.xpath("//h1[normalize-space(text())='testTable']");
+    private final By profileAvatar = By.id("header-member-menu-avatar");
+
     private final String tableTitle = "testTable";
 
     public BoardsPage(WebDriver driver){
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        PageFactory.initElements(driver, this);
+        super(driver);
     }
 
     public void createBoard(){
         Allure.step("Click create button");
-        createButton.click();
+        click(createButton);
 
         Allure.step("Click create table option");
-        selectBoardType.click();
+        click(selectBoardType);
 
         Allure.step("Enter table name: " + tableTitle);
-        tableNameInputField.sendKeys(tableTitle);
+        type(tableNameInputField, tableTitle);
 
         Allure.step("Wait for and click create to be clickable");
-        wait.until(ExpectedConditions.elementToBeClickable(createBoard)).click();
+        click(createBoard);
 
         Allure.step("Verify board name is displayed");
-        wait.until(ExpectedConditions.visibilityOf(tableName));
-        Assert.assertTrue("Table name is not shown", tableName.isDisplayed());
+        isVisible(tableName);
     }
 
     public boolean isUserLoggedIn(){
-        wait.until(ExpectedConditions.visibilityOf(profileAvatar));
-        return profileAvatar.isDisplayed();
+        return isVisible(profileAvatar);
     }
 }
